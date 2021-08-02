9d72433076ad21345fdb31d507a7a493d969c25b
github username <github email address>
2021-08-01 22:56:30 -0400

    commit tree a610ff557d41012afc329b677d73700bad918834

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg){
+  document.getElementById("greeting").innerHTML = msg;
+}
+
+
+function greet(input){
+
+  const hour= parseInt(input);
+  if ( hour < 12) return "Good Morning";
+  if ( hour > 17) return "Good Evening";
+  return "Good Afternoon"
+  
+}
